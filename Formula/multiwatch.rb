e48642588b603e1a3948dc62b21c9e9eaf32c4b1
Vadim Shashin <enapiuz@gmail.com>
2019-05-15 10:58:03 +0300

    multiwatch 0.4.0

--- Formula/multiwatch.rb
+++ Formula/multiwatch.rb
@@ -1,9 +1,9 @@
 class Multiwatch < Formula
   desc "Simple task runner on directory changes"
   homepage "https://github.com/Enapiuz/multiwatch"
-  url "https://github.com/Enapiuz/multiwatch/releases/download/0.3.0/multiwatch_0.3.0_Darwin_x86_64.tar.gz"
-  sha256 "8140cfdf9e863b92326da645161872f723f3d6a6b2d1a7a8e5cc612d67dbd86d"
-  version "0.3.0"
+  url "https://github.com/Enapiuz/multiwatch/releases/download/0.4.0/multiwatch_0.4.0_Darwin_x86_64.tar.gz"
+  sha256 "4a9f93ba2c7e8ebd4669fc86ffe2994825695cde34b6917b724917b12dfb0c15"
+  version "0.4.0"
 
   bottle :unneeded
 
